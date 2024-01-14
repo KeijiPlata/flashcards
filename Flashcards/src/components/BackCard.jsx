d99d4efdbd6739210d51088fc1c08e9fafbf436e
KeijiPlata <lordmiackykeijip@gmail.com>
2024-01-15 02:03:19 +0800

    fix: card responsiveness fix: fix navigation arrow size

--- Flashcards/src/components/BackCard.jsx
+++ Flashcards/src/components/BackCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const BackCard = ({ handleFlip, text }) => {
   return (
     <div
-      className="w-96 h-96 bg-blue-500 text-3xl text-white rounded-md text-center p-10"
+      className="w-full h-full bg-blue-500 text-3xl text-white rounded-md text-center p-10 flex justify-center items-center"
       onClick={handleFlip}
     >
       <h2>{text}</h2>
--- Flashcards/src/pages/Flashcardspage.jsx
+++ Flashcards/src/pages/Flashcardspage.jsx
@@ -3,14 +3,19 @@ import ReactCardFlip from "react-card-flip";
 import FrontCard from "../components/FrontCard";
 import BackCard from "../components/BackCard";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import "swiper/css";
+import "./styles.css"
 
 export default function Flashcardspage() {
   const data = [
-    { textFront: "front1", textBack: "back1" },
+    { textFront: "HTML stands for?", textBack: "Hypertext Markup Language" },
     { textFront: "front2", textBack: "back2" },
+    { textFront: "front3", textBack: "back3" },
   ];
-  const [flips, setFlips] = useState(Array(data.length).fill(false)); // Initialize an array of flip states
+  const [flips, setFlips] = useState(Array(data.length).fill(false));
 
   const handleFlip = (index) => {
     const newFlips = [...flips];
@@ -18,34 +23,49 @@ export default function Flashcardspage() {
     setFlips(newFlips);
   };
   return (
-    <div className="bg-red-500 h-screen w-full flex justify-center items-center">
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        {data.map((text, index) => (
-          <SwiperSlide key={index}>
-            <div className="flex justify-center items-center">
-              <ReactCardFlip
-                isFlipped={flips[index]}
-                flipDirection="horizontal"
-                containerClassName="font-bold"
-              >
-                <FrontCard
-                  handleFlip={() => handleFlip(index)}
-                  text={text.textFront}
-                />
-                <BackCard
-                  handleFlip={() => handleFlip(index)}
-                  text={text.textBack}
-                />
-              </ReactCardFlip>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <div className="h-screen w-full">
+      <div className="bg-red-500 h-full w-full flex justify-center items-center relative md:p-5 md:pt-10 pt-8">
+        <Swiper
+          pagination={{
+            type: "fraction",
+            el: ".swiper-pagination",
+            clickable: true,
+          }}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          }}
+          modules={[Pagination, Navigation]}
+          className="h-full w-full"
+        >
+          {data.map((text, index) => (
+            <SwiperSlide key={index}>
+              <div className="flex justify-center items-start h-full w-full">
+                <ReactCardFlip
+                  isFlipped={flips[index]}
+                  flipDirection="horizontal"
+                  containerClassName="font-bold w-10/12 h-5/6"
+                >
+                  <FrontCard
+                    handleFlip={() => handleFlip(index)}
+                    text={text.textFront}
+                  />
+                  <BackCard
+                    handleFlip={() => handleFlip(index)}
+                    text={text.textBack}
+                  />
+                </ReactCardFlip>
+              </div>
+            </SwiperSlide>
+          ))}
+           <div className="swiper-pagination text-white md:bottom-6 bottom-10"></div>
+          <div className="flex justify-between absolute w-full h-12 px-4 md:bottom-7 bottom-8 text-5xl">
+            <div  className="swiper-button-prev text-green-500 md:left-32 left-10"></div>
+            <div  className="swiper-button-next text-blue-500 md:right-32 right-10"></div>
+          </div>
+         
+        </Swiper>
+      </div>
     </div>
   );
 }
